39bf476d40f52be4a3fb181547fc408ad27c5a86
Juan Mosquella <jmosquella11@gmail.com>
2022-07-14 16:29:26 -0300

    dando estilos

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -13,41 +13,11 @@ const Item = ({ product}) => {
     <div>      
              
           <Link to={`/item/${product.id}`}>
-            <Card className='card-item' sx={{ 
-                maxHeight:'320px', maxWidth:'220px', transition:'.2s ease', borderRadius:'7px', 
-              }}>
-              <CardContent>                 
-                  <CardMedia className='item-img'                    
-                                          
-                      
-                      
-                      component="img"
-                      image={product.pictureURL}
-                      alt={product.title}
-                      
-                  />   
-                  <Typography sx={{ 
-                      fontSize:'1.3rem',
-                      marginBottom:'1rem'
+            <figure className='card-item' >
+              <img src={product.pictureURL} alt={product.title} />
                     
-                      }} variant="h4" component="div">
-                    {product.title}
-                  </Typography>
-                  <Typography sx={{ fontSize: 17 }} color="text.secondary" gutterBottom>
-                    $ {product.price}
-                  </Typography>             
-                  
-              </CardContent> 
-                    
-            </Card>              
-          </Link>
-      
-      
-      
-
-        
-      
-      
+            </figure>              
+          </Link>      
 
     </div>
   )
