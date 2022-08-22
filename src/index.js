c5a1efe2fe5bb0acf936be9fcb17b76afe67e062
Juan Mosquella <jmosquella11@gmail.com>
2022-08-22 14:17:41 -0300

    actualizado firebase

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import App from './App';
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAY2IYaMJmlZgjDTveYegItFfJRLX5ozZQ",
-    authDomain: "e-commerce-mosquella.firebaseapp.com",
-    projectId: "e-commerce-mosquella",
-    storageBucket: "e-commerce-mosquella.appspot.com",
-    messagingSenderId: "546138265758",
-    appId: "1:546138265758:web:3d8780763c7bdb232a838f"
+  apiKey: "AIzaSyAY2IYaMJmlZgjDTveYegItFfJRLX5ozZQ",
+  authDomain: "e-commerce-mosquella.firebaseapp.com",
+  projectId: "e-commerce-mosquella",
+  storageBucket: "e-commerce-mosquella.appspot.com",
+  messagingSenderId: "546138265758",
+  appId: "1:546138265758:web:9ac89f4aaa9041282a838f"
 };
 
 initializeApp(firebaseConfig);
